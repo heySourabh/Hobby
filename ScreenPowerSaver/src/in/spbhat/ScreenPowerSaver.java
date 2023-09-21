8142f27f746bc65da547131a116203eb80418a0e
Sourabh Bhat <heySourabh@gmail.com>
2023-09-21 16:12:38 +0200

    Exit if just one screen is connected

--- ScreenPowerSaver/src/in/spbhat/ScreenPowerSaver.java
+++ ScreenPowerSaver/src/in/spbhat/ScreenPowerSaver.java
@@ -8,18 +8,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static in.spbhat.util.Sleeper.sleepFor;
+import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static javax.swing.JOptionPane.WARNING_MESSAGE;
 
 public class ScreenPowerSaver {
     public static void main(String[] args) {
         Map<GraphicsDevice, Duration> screenIdleTime = new HashMap<>(3);
         Map<GraphicsDevice, JWindow> screenCoveringWindows = new HashMap<>(3);
-        Duration durationBetweenChecks = Duration.ofSeconds(1);
-        Duration maxIdleSeconds = Duration.ofSeconds(60);
+        Duration durationBetweenChecks = Duration.of(500, MILLIS);
+        Duration maxIdleDuration = Duration.of(2, MINUTES);
+        GraphicsDevice[] screens = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+        if (screens.length == 1) {
+            String msg = "ScreenPowerSaver: Exiting as only single screen is connected.";
+            System.out.println(msg);
+            JOptionPane.showMessageDialog(null, msg, "Only one screen", WARNING_MESSAGE);
+            System.exit(0);
+        }
         boolean running = true;
         while (running) {
             sleepFor(durationBetweenChecks);
             Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
-            GraphicsDevice[] screens = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
             for (GraphicsDevice screen : screens) {
                 Rectangle screenBounds = screen.getDefaultConfiguration().getBounds();
                 if (screenBounds.contains(mouseLocation)) {
@@ -40,7 +49,7 @@ public class ScreenPowerSaver {
             for (var screenWindowEntry : screenCoveringWindows.entrySet()) {
                 GraphicsDevice screen = screenWindowEntry.getKey();
                 JWindow window = screenWindowEntry.getValue();
-                if (maxIdleSeconds.compareTo(screenIdleTime.get(screen)) < 0) {
+                if (maxIdleDuration.compareTo(screenIdleTime.get(screen)) < 0) {
                     showBlank(window, screen);
                 } else {
                     hideBlank(window, screen);
@@ -60,7 +69,7 @@ public class ScreenPowerSaver {
             float stepsSize = 0.1f;
             for (float opacity = 1.0f; opacity > 0.0f; opacity -= stepsSize) {
                 window.setOpacity(opacity);
-                sleepFor(Duration.ofMillis(50));
+                sleepFor(Duration.of(20, MILLIS));
             }
             window.setOpacity(0.0f);
             window.setVisible(false);
@@ -81,7 +90,7 @@ public class ScreenPowerSaver {
             float stepSize = 0.1f;
             for (float opacity = 0.0f; opacity < 1.0f; opacity += stepSize) {
                 window.setOpacity(opacity);
-                sleepFor(Duration.ofMillis(50));
+                sleepFor(Duration.of(50, MILLIS));
             }
             window.setOpacity(1.0f);
             if (screen.isFullScreenSupported()) {
@@ -90,4 +99,3 @@ public class ScreenPowerSaver {
         }).start();
     }
 }
-
